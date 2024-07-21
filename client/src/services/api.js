59ebecefac4307bec4353f617296d3a87b94ec6c
Rohan Raidani <raidanirohan409@gmail.com>
2024-07-21 18:30:35 +0530

    link update

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
-const API_URL = "http://localhost:8000";
+// const new = " https://walletwise.onrender.com "
+
+// const API_URL = "http://localhost:8000";
+
+const API_URL = "https://walletwise.onrender.com";
 
 export const fetchTransactions = async () => {
     try{
